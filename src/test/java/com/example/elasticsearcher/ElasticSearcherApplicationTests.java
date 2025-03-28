5b6bc5f62354d270ad028969aca2e88a796e4bb4
mahboob <mahboobdorali1378@gmail.com>
2025-03-28 18:38:08 +0330

    initial commit

---
+++ src/test/java/com/example/elasticsearcher/ElasticSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
